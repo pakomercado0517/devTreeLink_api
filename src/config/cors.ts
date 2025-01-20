b38b7e7e9f494ca4a2c70864e28afd76e6b9bc9d
pakomercado0517 <sissyyayle0517@hotmail.com>
2025-01-20 00:10:31 -0600

    add url to cors whitelist

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,7 @@ const { FRONTEND_URL } = process.env;
 
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    const whiteList = [FRONTEND_URL];
+    const whiteList = [FRONTEND_URL, "http://localhost:5173"];
     if (process.argv[2] === "--api") {
       whiteList.push(undefined); // Permite solicitudes sin 'Origin'
     }
